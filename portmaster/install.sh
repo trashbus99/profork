7d0ec057136fb9c5a01e28b13fb1f77db0e134bd
Cliffy <csmandrew@live.com>
2025-03-17 21:24:55 +0200

    Update install.sh

--- portmaster/install.sh
+++ portmaster/install.sh
@@ -3,7 +3,7 @@
 dialog --msgbox "Portmaster will be installed to ports. It will usually launch on the second launch attempt.\n\nGame compatibility varies." 10 60
 cd /userdata/system
 rm -f Install.Full.PortMaster.sh
-wget https://github.com/PortsMaster/PortMaster-GUI/releases/download/2024.10.16-1432/Install.Full.PortMaster.sh
+wget https://github.com/PortsMaster/PortMaster-GUI/releases/download/2025.03.07-1324/Install.Full.PortMaster.sh
 chmod +x Install.Full.PortMaster.sh
 (./Install.Full.PortMaster.sh)
 rm Install.Full.PortMaster.sh
