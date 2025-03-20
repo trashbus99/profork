bc1a70c16219a05c0b63e0893ea0c626126ef174
Cliffy <csmandrew@live.com>
2025-03-20 14:35:52 +0200

    Update chiaki-arm64.sh

--- chiaki/chiaki-arm64.sh
+++ chiaki/chiaki-arm64.sh
@@ -39,7 +39,7 @@ APPLINK=$(curl -s https://api.github.com/repos/streetpea/chiaki-ng/releases/late
 APPHOME="GITHUB/STREETPEA"
 #---------------------------------------------------------------------
 # DEFINE LAUNCHER COMMAND >>
-COMMAND='mkdir /userdata/system/pro/'$appname'/home 2>/dev/null; mkdir /userdata/system/pro/'$appname'/config 2>/dev/null; mkdir /userdata/system/pro/'$appname'/roms 2>/dev/null; LD_LIBRARY_PATH="/userdata/system/pro/.dep:${LD_LIBRARY_PATH}" HOME=/userdata/system/pro/'$appname'/home XDG_CONFIG_HOME=/userdata/system/pro/'$appname'/config QT_SCALE_FACTOR="1" GDK_SCALE="1" XDG_DATA_HOME=/userdata/system/pro/'$appname'/home DISPLAY=:0.0 /userdata/system/pro/'$appname'/'$appname'.AppImage --appimage-extract-and-run'
+COMMAND='mkdir /userdata/system/pro/'$appname'/home 2>/dev/null; mkdir /userdata/system/pro/'$appname'/config 2>/dev/null; mkdir /userdata/system/pro/'$appname'/roms 2>/dev/null; LD_LIBRARY_PATH="/userdata/system/pro/.dep:${LD_LIBRARY_PATH}" HOME=/userdata/system/pro/'$appname'/home XDG_CONFIG_HOME=/userdata/system/pro/'$appname'/config QT_SCALE_FACTOR="1" GDK_SCALE="1" XDG_DATA_HOME=/userdata/system/pro/'$appname'/home DISPLAY=:0.0 /userdata/system/pro/'$appname'/'$appname'.AppImage --appimage-extract-and-run --no-sandbox'
 #--------------------------------------------------------------------- 
 ######################################################################
 ######################################################################
