ac4c3216d6df2243b3ebc37a359c6778c1e7af92
Cliffy <csmandrew@live.com>
2025-03-21 11:29:08 +0200

    +

--- app/Profork.sh
+++ app/Profork.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L https://github.com/trashbus99/profork/raw/master/app/mainmenu.sh | bash" 
 
+# Set path where .dialogrc should be
+DIALOGRC_PATH="/userdata/system/pro/.dialogrc"
+
+# Check if .dialogrc exists, if not, download it
+if [ ! -f "$DIALOGRC_PATH" ]; then
+    echo "Downloading .dialogrc for dialog color customization..."
+    mkdir -p /userdata/system/pro
+    curl -Ls https://github.com/trashbus99/profork/raw/master/.dep/.dialogrc -o "$DIALOGRC_PATH"
+fi
+
+# Launch the main menu script using xterm with custom dialog colors
+DIALOGRC="$DIALOGRC_PATH" \
+DISPLAY=:0.0 \
+xterm -fs 20 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 \
+-e bash -c "DISPLAY=:0.0 curl -L https://github.com/trashbus99/profork/raw/master/app/mainmenu.sh | bash"
