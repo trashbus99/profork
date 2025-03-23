cea2e9dd76dada8a1a9bc55dcf86339029644b9b
Cliffy <csmandrew@live.com>
2025-03-23 23:31:10 +0200

    +

--- windows/stt.sh
+++ windows/stt.sh
@@ -4,7 +4,7 @@
 URL="#!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/trashbus99/profork/releases/download/r1/Apotris.wsquashfs"
+URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/sonic-triple-trouble.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
