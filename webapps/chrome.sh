f4a0099198a9595a0fb81cf258da32c02209eab7
Cliffy <csmandrew@live.com>
2025-04-06 23:07:03 +0300

    +

--- webapps/chrome.sh
+++ webapps/chrome.sh
@@ -74,7 +74,7 @@ batocera-mouse show
           --bind /userdata/system/flatpak /var/lib/flatpak \
           --bind /userdata/system/etc/passwd /etc/passwd \
           --bind /userdata/system/etc/group /etc/group \
-          --bind /var/run/nvidia /var/run/nvidia \
+          --bind /var/run/nvidia /run/nvidia \
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
