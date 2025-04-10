0610cf5848ece497562fbd190e2f4c322276eb25
Cliffy <csmandrew@live.com>
2025-04-10 18:50:45 +0300

    +

--- .dep/.ytrk/b.sh
+++ .dep/.ytrk/b.sh
@@ -30,4 +30,4 @@ fi
 DIALOGRC="$DIALOGRC_PATH" \
 DISPLAY=:0.0 \
 xterm -fs 20 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 \
--e bash -c "DISPLAY=:0.0 curl -Ls https://github.com/DTJW92/batocera-unofficial-addons/raw/main/app/batocera-unofficial-addons-arm64.sh | bash"
+-e bash -c "DISPLAY=:0.0 curl -Ls curl -Ls install.batoaddons.app | bash"
